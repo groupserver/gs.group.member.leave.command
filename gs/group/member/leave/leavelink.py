57cf996df5302fbaaa07cd3b7520d0de37675cea
Michael JasonSmith <mpj17@onlinegroups.net>
2011-05-23 09:43:01 +1000

    Linking to the Leave page from the group homepage using the a viewlet.

---
+++ gs/group/member/leave/leavelink.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+from zope.cachedescriptors.property import Lazy
+from gs.group.member.base.viewlet import MemberViewlet
+
+class LeaveLinkViewlet(MemberViewlet):
+    def __init__(self, group, request, view, manager):
+        MemberViewlet.__init__(self, group, request, view, manager)
+
+    @Lazy
+    def show(self):
+        retval = self.isMember
+        return retval
+
