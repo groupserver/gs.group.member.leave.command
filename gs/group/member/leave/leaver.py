4ff9a9245659a276e9657fec587f61e41e44b609
Michael JasonSmith <mpj17@onlinegroups.net>
2012-02-24 16:23:36 +1100

    Adding the group-leave event.

--- gs/group/member/leave/leaver.py
+++ gs/group/member/leave/leaver.py
@@ -1,15 +1,18 @@
 # coding=utf-8
 '''The code that removes a group member from the group'''
 from zope.component import createObject
+from zope.event import notify
 from Products.GSGroupMember.groupMembersInfo import GSGroupMembersInfo
 from Products.GSGroupMember.groupmembershipstatus import GSGroupMembershipStatus
 from gs.profile.notify.interfaces import IGSNotifyUser
 from gs.group.member.base.utils import member_id
 from gs.group.member.leave.utils import removeAllPositions
 from gs.group.member.leave.audit import LeaveAuditor, LEAVE
+from event import GSLeaveGroupEvent
 
 class GroupLeaver(object):
-    def __init__(self, groupInfo, userInfo):
+    def __init__(self, context, groupInfo, userInfo):
+        self.context = context
         self.groupInfo = groupInfo
         self.userInfo = userInfo
 
@@ -34,12 +37,14 @@ class GroupLeaver(object):
         usergroupName = member_id(gId)
         retval = removeAllPositions(self.groupInfo, self.userInfo)
         self.userInfo.user.del_groupWithNotification(usergroupName)
+        groupObj = self.groupInfo.groupObj
         if not self.isMember:
-            auditor = LeaveAuditor(self.groupInfo.groupObj, self.userInfo, self.groupInfo)
+            auditor = LeaveAuditor(groupObj, self.userInfo, self.groupInfo)
             auditor.info(LEAVE)
             for admin in adminsToNotify:
                 admin.send_notification('leave_group_admin', gId, nDict)
             retval.append('removed from the group')
+        notify(GSLeaveGroupEvent(groupObj, self.groupInfo, self.userInfo))
         return retval
 
     def adminNotification(self):
