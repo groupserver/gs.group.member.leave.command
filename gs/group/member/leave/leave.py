e9bc60a37bbdf96f67cffb135736633715d4a4a3
Michael JasonSmith <mpj17@onlinegroups.net>
2012-02-24 16:23:05 +1100

    A tidy up for the leave page.

--- gs/group/member/leave/leave.py
+++ gs/group/member/leave/leave.py
@@ -1,43 +1,41 @@
 # coding=utf-8
 '''The form that allows a group member to leave a group'''
-try:
-    from five.formlib.formbase import PageForm
-except ImportError:
-    from Products.Five.formlib.formbase import PageForm
 from zope.formlib import form
 from zope.formlib.form import Fields
+from zope.cachedescriptors.property import Lazy
 from zope.component import createObject
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
-from Products.GSContent.interfaces import IGSSiteInfo
 from gs.content.form.radio import radio_widget
 from Products.GSGroup.groupInfo import GSGroupInfo
 from Products.GSGroup.joining import GSGroupJoining
-from gs.group.member.leave.leaver import GroupLeaver
-from gs.group.member.leave.fields import LeaveFields
+from gs.content.form.form import SiteForm
+from leaver import GroupLeaver
+from fields import LeaveFields
 
-class LeaveForm(PageForm):
+class LeaveForm(SiteForm):
     pageTemplateFileName = 'browser/templates/leave.pt'
     template = ZopeTwoPageTemplateFile(pageTemplateFileName)
     
-    def __init__(self, context, request):
-        PageForm.__init__(self, context, request)
-        self.context = context
-        self.siteInfo = IGSSiteInfo(context)
-        groupId = request.form['groupId']
-        self.groupInfo = GSGroupInfo(context, groupId)
-        self.leaveFields = LeaveFields(self.groupInfo)
-        self.form_fields = Fields(self.leaveFields.fields)
-        self.form_fields['changeSubscription'].custom_widget = radio_widget
+    @Lazy
+    def groupInfo(self):
+        groupId = self.request.form['groupId']
+        return GSGroupInfo(self.context, groupId)
+    
+    @Lazy
+    def leaveFields(self):
+        return LeaveFields(self.groupInfo)
+    
+    @Lazy
+    def form_fields(self):
+        retval = Fields(self.leaveFields.fields)
+        retval['changeSubscription'].custom_widget = radio_widget
+        return retval
 
-    @property
-    def userInfo(self):
-        return createObject('groupserver.LoggedInUser', self.context)
-
-    @property
+    @Lazy
     def groupLeaver(self):
-        return GroupLeaver(self.groupInfo, self.userInfo)
+        return GroupLeaver(self.context, self.groupInfo, self.loggedInUser)
 
-    @property
+    @Lazy
     def label(self):
         retval = u'Left Group'
         if self.groupLeaver:
@@ -45,9 +43,8 @@ class LeaveForm(PageForm):
         return retval
     
     def setUpWidgets(self, ignore_request=False):
-        self.widgets = form.setUpWidgets(
-            self.form_fields, self.prefix, self.context,
-            self.request, form=self,
+        self.widgets = form.setUpWidgets(self.form_fields, self.prefix, 
+            self.context, self.request, form=self, 
             ignore_request=ignore_request)
         self.widgets['changeSubscription']._displayItemForMissingValue = False
     
@@ -82,7 +79,7 @@ class LeaveForm(PageForm):
         digest = 'digest'
         assert change in [web, digest], \
           'Subscription change must be to web or digest'
-        user = self.userInfo.user
+        user = self.loggedInUser.user
         if change == digest:
             user.set_enableDigestByKey(self.groupInfo.id)
             status = u'The posts from %s will now be delivered '\
@@ -93,5 +90,4 @@ class LeaveForm(PageForm):
             status = u'You will no longer receive any posts from %s via email.' % \
               self.groupInfo.name
         return status
-    
-    
+
