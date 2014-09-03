58f111802062e7862c62af4caf76b34aa001e47e
Michael JasonSmith <mpj17@onlinegroups.net>
2014-09-03 15:26:14 -0700

    Updating the product-deps

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-##############################################################################
+############################################################################
 #
 # Copyright © 2013, 2014 OnlineGroups.net and Contributors.
 # All Rights Reserved.
@@ -11,7 +11,7 @@
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE.
 #
-##############################################################################
+############################################################################
 import codecs
 import os
 from setuptools import setup, find_packages
@@ -21,57 +21,65 @@ version = get_version()
 
 with codecs.open('README.txt', encoding='utf-8') as f:
     long_description = f.read()
-with codecs.open(os.path.join("docs", "HISTORY.txt"), encoding='utf-8') as f:
+with codecs.open(os.path.join("docs", "HISTORY.txt"),
+                 encoding='utf-8') as f:
     long_description += '\n' + f.read()
 
 setup(name='gs.group.member.leave',
-    version=version,
-    description="The pages related to a member leaving a group",
-    long_description=long_description,
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        "Environment :: Web Environment",
-        "Framework :: Zope2",
-        "Intended Audience :: Developers",
-        'License :: OSI Approved :: Zope Public License',
-        "Natural Language :: English",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    keywords='user, group, member, group member, leave',
-    author='Alice Murphy',
-    author_email='alice@onlinegroups.net',
-    maintainer='Michael JasonSmith',
-    maintainer_email='mpj17@onlinegroups.net',
-    url='https://source.iopen.net/groupserver/gs.group.member.leave/',
-    license='ZPL 2.1',
-    packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.group', 'gs.group.member'],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'setuptools',
-        'pytz',
-        'zope.cachedescriptors',
-        'zope.component',
-        'zope.event',
-        'zope.formlib',
-        'zope.interface',
-        'zope.schema',
-        'Zope2',
-        'gs.content.form.base',
-        'gs.content.layout',
-        'gs.group.member.base',
-        'gs.group.member.manage',
-        'gs.profile.notify',
-        'gs.group.member.base',
-        'gs.group.member.manage',
-        'Products.GSAuditTrail',
-        'Products.GSGroup',
-        'Products.XWFCore',
-    ],
-    entry_points="""
-    # -*- Entry points: -*-
-    """,
-)
+      version=version,
+      description="The pages related to a member leaving a group",
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          "Environment :: Web Environment",
+          "Framework :: Zope2",
+          "Intended Audience :: Developers",
+          'License :: OSI Approved :: Zope Public License',
+          "Natural Language :: English",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='user, group, member, group member, leave',
+      author='Alice Murphy',
+      author_email='alice@onlinegroups.net',
+      maintainer='Michael JasonSmith',
+      maintainer_email='mpj17@onlinegroups.net',
+      url='https://source.iopen.net/groupserver/gs.group.member.leave/',
+      license='ZPL 2.1',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['gs', 'gs.group', 'gs.group.member'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'pytz',
+          'zope.browserpage',
+          'zope.cachedescriptors',
+          'zope.component',
+          'zope.event',
+          'zope.formlib',
+          'zope.interface',
+          'zope.schema',
+          'zope.tal',
+          'zope.tales',
+          'Zope2',
+          'gs.content.email.base',
+          'gs.content.form.base',
+          'gs.content.layout',
+          'gs.core',
+          'gs.group.list.command',
+          'gs.group.member.base',
+          'gs.group.member.manage',
+          'gs.group.privacy',
+          'gs.profile.notify',
+          'gs.group.member.base',
+          'gs.group.member.manage',
+          'Products.CustomUserFolder',
+          'Products.GSAuditTrail',
+          'Products.GSGroup',
+          'Products.GSGroupMember',
+          'Products.XWFCore',
+      ],
+      entry_points="""# -*- Entry points: -*-
+      """,)
