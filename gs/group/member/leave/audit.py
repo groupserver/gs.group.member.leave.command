d7739805f7649c6e350693aca11920665ba1a4c0
Michael JasonSmith <mpj17@onlinegroups.net>
2014-09-04 09:23:12 -0700

    Switching to Unicode literals throughout, and (hopefully) the last of the PEP-8 updates.

--- gs/group/member/leave/audit.py
+++ gs/group/member/leave/audit.py
@@ -1,4 +1,20 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+############################################################################
+#
+# Copyright © 2010, 2011, 2012, 2013, 2014 OnlineGroups.net and
+# Contributors.
+#
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+############################################################################
+from __future__ import absolute_import, unicode_literals
 from pytz import UTC
 from datetime import datetime
 from zope.cachedescriptors.property import Lazy
@@ -10,11 +26,9 @@ from Products.CustomUserFolder.userinfo import userInfo_to_anchor
 from Products.GSGroup.groupInfo import groupInfo_to_anchor
 from Products.GSAuditTrail import IAuditEvent, BasicAuditEvent, AuditQuery
 from Products.GSAuditTrail.utils import event_id_from_data
-
 SUBSYSTEM = 'gs.group.member.leave'
-import logging
-log = logging.getLogger(SUBSYSTEM)
-
+from logging import getLogger
+log = getLogger(SUBSYSTEM)
 UNKNOWN = '0'  # Unknown is always "0"
 LEAVE = '1'
 
@@ -24,23 +38,23 @@ class LeaveAuditEventFactory(object):
     """
     implements(IFactory)
 
-    title = u'GroupServer Leave Group Audit Event Factory'
-    description = u'Creates a GroupServer event auditor for leaving groups'
+    title = 'GroupServer Leave Group Audit Event Factory'
+    description = 'Creates a GroupServer event auditor for leaving groups'
 
-    def __call__(self, context, event_id, code, date,
-        userInfo, instanceUserInfo, siteInfo, groupInfo,
-        instanceDatum='', supplementaryDatum='', subsystem=''):
-        """Create an event
-        """
+    def __call__(self, context, event_id, code, date, userInfo,
+                 instanceUserInfo, siteInfo, groupInfo, instanceDatum='',
+                 supplementaryDatum='', subsystem=''):
+        """Create an event"""
         assert subsystem == SUBSYSTEM, 'Subsystems do not match'
 
         if (code == LEAVE):
-            event = LeaveEvent(context, event_id, date,
-              userInfo, instanceUserInfo, siteInfo, groupInfo)
+            event = LeaveEvent(context, event_id, date, userInfo,
+                               instanceUserInfo, siteInfo, groupInfo)
         else:
             event = BasicAuditEvent(context, event_id, UNKNOWN, date,
-              userInfo, instanceUserInfo, siteInfo, groupInfo,
-              instanceDatum, supplementaryDatum, SUBSYSTEM)
+                                    userInfo, instanceUserInfo, siteInfo,
+                                    groupInfo, instanceDatum,
+                                    supplementaryDatum, SUBSYSTEM)
         assert event
         return event
 
@@ -55,57 +69,56 @@ class LeaveEvent(BasicAuditEvent):
     implements(IAuditEvent)
 
     def __init__(self, context, id, d, userInfo, instanceUserInfo,
-                  siteInfo, groupInfo):
-        """Create a leave event
-        """
-        BasicAuditEvent.__init__(self, context, id, LEAVE, d, userInfo,
-          instanceUserInfo, siteInfo, groupInfo, None, None, SUBSYSTEM)
+                 siteInfo, groupInfo):
+        """Create a leave event"""
+        super(LeaveEvent, self).__init__(context, id, LEAVE, d, userInfo,
+                                         instanceUserInfo, siteInfo,
+                                         groupInfo, None, None, SUBSYSTEM)
 
     @property
     def adminRemoved(self):
         retval = False
-        if self.userInfo.id and self.userInfo.id != self.instanceUserInfo.id:
+        if (self.userInfo.id and
+           (self.userInfo.id != self.instanceUserInfo.id)):
             retval = True
         return retval
 
-    def __str__(self):
+    def __unicode__(self):
         if self.adminRemoved:
-            r = u'{0} ({1}) was removed from {2} ({3}) on {4} ({5}) by {6} '\
-                u'({7}).'
-            retval = r.format(self.instanceUserInfo.name,
-                        self.instanceUserInfo.id, self.groupInfo.name,
-                        self.groupInfo.id, self.siteInfo.name,
-                        self.siteInfo.id, self.userInfo.name,
-                        self.userInfo.id)
+            r = '{0} ({1}) was removed from {2} ({3}) on {4} ({5}) by {6} '\
+                '({7}).'
+            retval = r.format(
+                self.instanceUserInfo.name,
+                self.instanceUserInfo.id, self.groupInfo.name,
+                self.groupInfo.id, self.siteInfo.name,
+                self.siteInfo.id, self.userInfo.name,
+                self.userInfo.id)
         else:
-            retval = u'%s (%s) left %s (%s) on %s (%s).' % (
+            retval = '%s (%s) left %s (%s) on %s (%s).' % (
                 self.instanceUserInfo.name, self.instanceUserInfo.id,
                 self.groupInfo.name, self.groupInfo.id,
                 self.siteInfo.name, self.siteInfo.id)
-        retval = retval.encode('ascii', 'ignore')
         return retval
 
     @property
     def xhtml(self):
-        cssClass = u'audit-event groupserver-group-member-%s' % \
-          self.code
+        cssClass = 'audit-event groupserver-group-member-%s' % self.code
         retval = ''
         # --=mpj17=-- Sometimes this is false. I do not know why.
         if self.groupInfo.id:
-            retval = u'<span class="%s">Left %s</span>' % \
+            retval = '<span class="%s">Left %s</span>' % \
                 (cssClass, groupInfo_to_anchor(self.groupInfo))
 
             if self.adminRemoved:
-                retval = u'%s &#8212; removed by %s' % \
-                            (retval, userInfo_to_anchor(self.userInfo))
-                retval = u'%s (%s)' % \
-                          (retval, munge_date(self.context, self.date))
+                retval = '%s &#8212; removed by %s' % \
+                    (retval, userInfo_to_anchor(self.userInfo))
+                retval = '%s (%s)' % \
+                    (retval, munge_date(self.context, self.date))
         return retval
 
 
 class LeaveAuditor(object):
-    """An Auditor for leaving
-    """
+    """An Auditor for leaving"""
     def __init__(self, context, instanceUserInfo, groupInfo=None):
         """Create a leaving auditor.
         """
@@ -126,7 +139,7 @@ class LeaveAuditor(object):
     @Lazy
     def groupInfo(self):
         retval = self.__groupInfo if self.__groupInfo is not None else \
-                  createObject('groupserver.GroupInfo', self.context)
+            createObject('groupserver.GroupInfo', self.context)
         return retval
 
     @Lazy
@@ -146,13 +159,14 @@ class LeaveAuditor(object):
             * Writes the event to the standard Python log.
         """
         d = datetime.now(UTC)
-        eventId = event_id_from_data(self.userInfo,
-          self.instanceUserInfo, self.siteInfo, code, instanceDatum,
-          '%s-%s' % (self.groupInfo.name, self.groupInfo.id))
+        eventId = event_id_from_data(
+            self.userInfo,
+            self.instanceUserInfo, self.siteInfo, code, instanceDatum,
+            '%s-%s' % (self.groupInfo.name, self.groupInfo.id))
 
-        e = self.factory(self.context, eventId, code, d,
-          self.userInfo, self.instanceUserInfo, self.siteInfo,
-          self.groupInfo, instanceDatum, None, SUBSYSTEM)
+        e = self.factory(self.context, eventId, code, d, self.userInfo,
+                         self.instanceUserInfo, self.siteInfo,
+                         self.groupInfo, instanceDatum, None, SUBSYSTEM)
 
         self.queries.store(e)
         log.info(e)
--- gs/group/member/leave/fields.py
+++ gs/group/member/leave/fields.py
@@ -1,4 +1,21 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+############################################################################
+#
+# Copyright © 2010, 2011, 2012, 2013, 2014 OnlineGroups.net and
+# Contributors.
+#
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+############################################################################
+from __future__ import absolute_import, unicode_literals
+from zope.cachedescriptors.property import Lazy
 from zope.schema import Choice
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from Products.GSGroup.joining import GSGroupJoining
@@ -7,34 +24,31 @@ from Products.GSGroup.joining import GSGroupJoining
 class LeaveFields(object):
     def __init__(self, groupInfo):
         self.groupInfo = groupInfo
-        self.__leaveTerm = self.__fields = self.__vocab = None
 
-    @property
+    @Lazy
     def fields(self):
-        if self.__fields is None:
-            self.__fields = Choice(
-              __name__=u'changeSubscription',
-              title=u'Want less email?',
-              description=u'These options are shown to a group member '
-                u'wishing to leave the group',
-              vocabulary=self.vocab,
-              default='leave',
-              required=False)
-        return self.__fields
+        retval = Choice(
+            __name__='changeSubscription',
+            title='Want less email?',
+            description='These options are shown to a group member wishing '
+                        'to leave the group',
+            vocabulary=self.vocab,
+            default='leave',
+            required=False)
+        return retval
 
     @property
     def vocab(self):
         retval = SimpleVocabulary([
-          self.leaveTerm,
-          SimpleTerm('web', 'web', u'Read posts on the web (no email)'),
-          SimpleTerm('digest', 'digest', u'Receive a digest of topics '
-            u'(maximum one email per day)')])
+            self.leaveTerm,
+            SimpleTerm('web', 'web', 'Read posts on the web (no email)'),
+            SimpleTerm('digest', 'digest', 'Receive a digest of topics '
+                       '(maximum one email per day)')])
         return retval
 
-    @property
+    @Lazy
     def leaveTerm(self):
-        if self.__leaveTerm is None:
-            rejoinAdvice = GSGroupJoining(self.groupInfo.groupObj).rejoin_advice
-            title = u'Leave %s (%s)' % (self.groupInfo.name, rejoinAdvice)
-            self.__leaveTerm = SimpleTerm('leave', 'leave', title)
-        return self.__leaveTerm
+        rejoinAdvice = GSGroupJoining(self.groupInfo.groupObj).rejoin_advice
+        title = 'Leave %s (%s)' % (self.groupInfo.name, rejoinAdvice)
+        retval = SimpleTerm('leave', 'leave', title)
+        return retval
