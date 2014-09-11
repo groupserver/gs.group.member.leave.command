d7d8dfb51217af597efe3ecef713aa1e73aec486
Michael JasonSmith <mpj17@onlinegroups.net>
2014-09-11 13:11:25 -0700

    Adding a MessageFactory

--- gs/group/member/leave/__init__.py
+++ gs/group/member/leave/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+from zope.i18nmessageid import MessageFactory
 #lint:disable
+GSMessageFactory = MessageFactory('gs.group.member.leave')
 from .leaver import GroupLeaver
 #lint:enable
